7f5067aeeeee40261485c5584e01cefe6336b21d
Docrob <mark.robinson@codeup.com>
2022-07-05 14:27:36 -0500

    remove jquery from message notification

---
+++ js/router.js
@@ -0,0 +1,93 @@
+import Home, {HomeEvents} from "./views/Home.js";
+import PostIndex from "./views/PostIndex.js";
+import About, {AboutEvents} from "./views/About.js";
+import Error404 from "./views/Error404.js";
+import Loading from "./views/Loading.js";
+import Login from "./views/Login.js";
+import LoginEvent from "./auth.js";
+import Register from "./views/Register.js"
+import {RegisterEvent} from "./views/Register.js";
+import {PostEvents} from "./views/PostIndex.js";
+import UserIndex, {UserEvents} from "./views/User.js";
+import Logout, {LogoutEvents} from "./views/Logout.js";
+
+/**
+ * Returns the route object for a specific route based on the given URI
+ * @param URI
+ * @returns {*}
+ */
+export default function router(URI) {
+    const routes = {
+        '/': {
+            returnView: Home,
+            state: {},
+            uri: '/',
+            title: 'Home',
+            viewEvent: HomeEvents
+        },
+        '/logout': {
+            returnView: Logout,
+            state: {},
+            uri: '/',
+            title: 'Logout',
+            viewEvent: LogoutEvents
+        },
+        '/login': {
+            returnView: Login,
+            state: {},
+            uri: '/login',
+            title: "Login",
+            viewEvent: LoginEvent
+        },
+        '/register': {
+            returnView: Register,
+            state: {},
+            uri: '/register',
+            title: 'Register',
+            viewEvent: RegisterEvent
+        },
+        '/users': {
+            returnView: UserIndex,
+            state: {
+                users: "/api/users/me"
+            },
+            uri: "/users",
+            title: 'User Info',
+            viewEvent: UserEvents
+        },
+        '/posts': {
+            returnView: PostIndex,
+            state: {
+                posts: '/api/posts',
+                categories: '/api/categories'
+            },
+            uri: '/posts',
+            title: 'All Posts',
+            viewEvent: PostEvents
+        },
+        '/about': {
+            returnView: About,
+            state: {},
+            uri: '/about',
+            title: 'About',
+            viewEvent: AboutEvents
+        },
+        '/error': {
+            returnView: Error404,
+            state: {},
+            uri: location.pathname,
+            title: ' ERROR',
+        },
+        '/loading': {
+            returnView: Loading,
+            state: {},
+            uri: location.pathname,
+            title: 'Loading...',
+        }
+    };
+
+    return routes[URI];
+
+    // if uri looks like /users/1
+
+}
---
+++ js/views/User.js
@@ -0,0 +1,62 @@
+import createView from "../createView.js";
+
+const BASE_URI = `${BACKEND_HOST}/api/users`;
+
+export default function UserIndex(props) {
+    console.log(props);
+    // language=HTML
+    return `
+        <header>
+            <h1>Your Page</h1>
+        </header>
+        <main>
+            <img src="${props.users.photourl}">
+
+            <form id="register-form">
+                <label for="username">Username</label>
+                <input disabled id="username" name="username" value="${props.users.username}" type="text"/><br>
+                <label for="email">Email</label>
+                <input disabled id="email" name="email" type="email" value="${props.users.email}"><br>
+                <label for="new-password">New Password</label>
+                <input id="new-password" name="new-password" type="password" value="this is not your real password"/><br>
+                <button id="change-password-button" type="button">Change Password</button>
+            </form>
+            <hr>
+            <h5>My Posts</h5>
+            ${props.users.posts.map(post => {
+                return `
+<div class="card">
+  <div class="card-header">
+    ${post.title}
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>${post.content}</p>
+    </blockquote>
+  </div>
+</div>`
+    }).join('')}
+        </main>
+    `;
+}
+
+export function UserEvents() {
+    $("#change-password-button").click(function() {
+        const id = 1; // $("#add-post-id").val();
+        let uriExtra = '/1/updatePassword';
+        const newPassword = $("#new-password").val()
+
+        const request = {
+            method: "PUT"
+        }
+        fetch(`${BASE_URI}${uriExtra}?newPassword=${newPassword}`, request)
+            .then(res => {
+                console.log(`${request.method} SUCCESS: ${res.status}`);
+            }).catch(error => {
+            console.log(`${request.method} ERROR: ${error}`);
+        }).finally(() => {
+            createView("/users");
+        });
+
+    });
+}
