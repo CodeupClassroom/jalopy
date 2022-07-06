8b6800cd4fdcd6a0fb1b8ee9d8b5f246cb48f829
Docrob <mark.robinson@codeup.com>
2022-07-06 11:52:03 -0500

    clean up about page

--- js/views/About.js
+++ js/views/About.js
@@ -1,15 +1,16 @@
 import {showNotification} from "../messaging.js";
 
 export default function About(props) {
+    // language=HTML
     return `
-        <header>
-            <h1>About Page</h1>
-        </header>
-        <main>
-            <div>
-                <p>About page is here.</p>  
-            </div>
-        </main>
+<header>
+    <h1>About Page</h1>
+</header>
+<main>
+    <div>
+        <p>About page is here.</p>  
+    </div>
+</main>
     `;
 }
 export function AboutEvents() {
