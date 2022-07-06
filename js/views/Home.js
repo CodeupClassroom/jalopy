902b6b0636208c0ccbb4d3aa6778ea0cb148886e
Docrob <mark.robinson@codeup.com>
2022-07-06 15:21:03 -0500

    exercise work

--- js/views/Home.js
+++ js/views/Home.js
@@ -2,21 +2,36 @@ import {getUser} from "../auth.js";
 
 const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 
+let imageNumber = 1;
+
 export default function Home(props) {
     return `
         <header>
-            <h1>Hello Jalopy</h1>
+            <h1 class="text-center">Hello Jalopy</h1>
         </header>
         <main>
             <div>
-                <img src="assets/jalopy.jpeg" />
+                <div id="img-container">
+                    <img id="jalopy-img" src="${INTELLIJ_PROJECT_PATH}assets/jalopy1.jpeg" alt="jalopy" />
+                </div>
                 <p>
-                    
-                </p>    
+                    Welcome to my Jalopy application! 
+                </p>
+                <button id="img-button">Change image</button>    
             </div>
         </main>
     `;
 }
 
+function changeImage() {
+    const img = document.querySelector("#jalopy-img");
+    img.setAttribute("src", `${INTELLIJ_PROJECT_PATH}assets/jalopy${imageNumber}.jpeg`);
+    imageNumber++;
+    if(imageNumber > 3) {
+        imageNumber = 1;
+    }
+}
+
 export function HomeEvents() {
+    document.querySelector("#img-button").addEventListener("click", changeImage);
 }
